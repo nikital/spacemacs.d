35d99398f131a5bb9bf28692feeb0a544c96def0
Nikita Leshenko <nikita@leshenko.net>
2017-08-16 00:16:54 +0300

    Add gtags and html

--- init.el
+++ init.el
@@ -59,6 +59,8 @@ values."
      evil-commentary
      company-neo
      nik-lisp
+     gtags
+     html
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
