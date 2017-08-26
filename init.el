c25722cec337f0a23516dca3f708522ca1dc3e3e
Nikita Leshenko <nikita@leshenko.net>
2017-08-26 23:32:58 +0300

    Move keybindings to keybindings.el

--- init.el
+++ init.el
@@ -326,9 +326,6 @@ This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
 
-  (spacemacs/set-leader-keys "bB" 'switch-to-buffer-other-window)
-  (spacemacs/set-leader-keys "fF" 'find-file-other-window)
-
   (defun set-edk2-formatting ()
     (interactive)
     (setq c-basic-offset 2)
---
+++ layers/nik/keybindings.el
@@ -0,0 +1,2 @@
+(spacemacs/set-leader-keys "bB" 'switch-to-buffer-other-window)
+(spacemacs/set-leader-keys "fF" 'find-file-other-window)
