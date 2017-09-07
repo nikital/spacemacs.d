30336ff3e4040b26ac39d8c21cfda1bef14e26b6
Nikita Leshenko <nikita@leshenko.net>
2017-09-07 21:46:22 +0300

    Search explicitly with Projectile because it gives better results

--- layers/nik/packages.el
+++ layers/nik/packages.el
@@ -11,7 +11,7 @@
   (bind-key "RET" 'save-some-buffers-no-confirm evil-normal-state-map)
   (bind-key "C-j" 'nik/evil-scroll-down evil-motion-state-map)
   (bind-key "C-k" 'nik/evil-scroll-up evil-motion-state-map)
-  (bind-key "C-p" 'project-find-file evil-normal-state-map))
+  (bind-key "C-p" 'projectile-find-file evil-normal-state-map))
 
 (defun nik/pre-init-company ()
   (spacemacs|use-package-add-hook company
