4f08651c183cf8b1cb53c8ce7ebe06b42e9d362c
Nikita Leshenko <nikita@leshenko.net>
2017-12-22 18:35:36 +0200

    Add C/C++

--- init.el
+++ init.el
@@ -63,6 +63,7 @@ values."
      nik-lisp
      gtags
      html
+     c-c++
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
